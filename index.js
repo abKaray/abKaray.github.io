b4f4b4a4cf1455719fcdd0d81381a548c97ee227
abKaray <32565746+abKaray@users.noreply.github.com>
2017-10-23 13:10:07 +0300

    update map

--- index.js
+++ index.js
@@ -113,3 +113,12 @@ $(".inp").blur(function () {
        $(this.nextElementSibling).removeClass("active");
    }
 });
+
+function initMap() {
+    var coordinates = {lat: 47.212325, lng: 38.933663},
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: coordinates
+        });
+}
+initMap();
